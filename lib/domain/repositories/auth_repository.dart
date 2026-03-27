8ee46ca40dd287330aac2be84d02f95f0a440393
Samet Ünsal <samet-unsal@hotmail.com>
2026-03-27 18:35:31 +0300

    chore: remove Firebase integration files

--- lib/domain/repositories/auth_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../../models/user_entity.dart';
-
-abstract class AuthRepository {
-  Stream<UserEntity?> get authStateChanges;
-  Future<UserEntity> signInAnonymously();
-  UserEntity? get currentUser;
-}
--- lib/domain/repositories/user_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../../models/user_entity.dart';
-
-abstract class UserRepository {
-  Future<void> createUserProfile(UserEntity user);
-  Future<void> updateUserLastSeen(String uid);
-  Future<UserEntity?> getUserProfile(String uid);
-}
